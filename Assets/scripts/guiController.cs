d98c5dfd16c995e33344684cbf7e0fe803236a05
Pin-Chia, Chen <seal789ie@gmail.com>
2015-01-12 17:53:50 +0800

    Menu

---
+++ Assets/scripts/guiController.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class guiController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void startClicked()
+	{
+		Application.LoadLevel("Main.unity");
+	}
+
+	public void exitClicked()
+	{
+		print("CLicked");
+		Application.Quit ();
+	}
+}
