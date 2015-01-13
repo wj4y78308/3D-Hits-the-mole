d223dc4382d0813b5c425419ab4512f28c5b1976
白紋愷 <101703015@nccu.edu.tw>
2015-01-13 15:16:44 +0800

    [update]

--- Assets/scripts/myGUI.cs
+++ Assets/scripts/myGUI.cs
@@ -120,7 +120,7 @@ public class myGUI : MonoBehaviour {
         }
 
         if (molesPop.gameOver)
-            GUI.Label(new Rect(Screen.width / 2 - 100, Screen.height / 2 - 50, 200, 100), "Game Over!\nPress 'R' restart!", guiStyle);
+            GUI.Label(new Rect(Screen.width / 2 - 100, Screen.height / 2 - 50, 200, 100), "Game Over!\n 'R' restart!", guiStyle);
     }
 
     void initGame() {
